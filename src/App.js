3a8e6eee6c46f2f37bc74ac8bebdcee5407717b4
beata <beata@emc2wave.com>
2019-10-23 15:31:42 -0700

    add cancel()

--- src/App.js
+++ src/App.js
@@ -11,13 +11,18 @@ function App() {
 
   useEffect(() => {
     setLoading(true)
-    axios.get(currentPageUrl)
+    let cancel;
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    })
     .then(res => {
       setLoading(false)
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p => p.name))
     })
+
+    return () =>  cancel()
   }, [currentPageUrl])
 
   if(loading) return 'Loading...'
