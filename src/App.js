29342f4316a537ac7c8e6c31d6e078c28aac8a98
beata <beata@emc2wave.com>
2019-10-23 11:21:41 -0700

    add useeffect

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PokemonList from './PokemonList'
 import axios from 'axios';
 
 function App() {
   const [pokemon, setPokemon] = useState([])
 
-  axios.get('https://pokeapi.co/api/v2/pokemon/')
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/')
     .then(res => {
       setPokemon(res.data.results.map(p => p.name))
     })
+  }, [])
+
+  
 
   return (
     <PokemonList pokemon={pokemon} />
